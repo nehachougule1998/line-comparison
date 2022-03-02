e3142b8ce14bd5b06919f352312cd15461324291
nehachougule1998 <nchougule734@gmail.com>
2022-03-02 14:41:16 +0530

    UC2 added

--- src/com/bridgelabz/Line_Comparison.java
+++ src/com/bridgelabz/Line_Comparison.java
@@ -2,19 +2,33 @@ package com.bridgelabz;
 import java.util.Scanner;
 public class Line_Comparison {
 
-	public static void main(String[] args) {
-		System.out.println("welcome to line comparison computation program on master branch");
-        Scanner ch = new Scanner(System.in);
-        System.out.println("enter the coordinates of x1 & y1 : ");
-        int x1 = ch.nextInt();
-        int y1 = ch.nextInt();
-        System.out.println("enter the coordinates of x2 & y2 : ");
-        int x2 = ch.nextInt();
-        int y2 = ch.nextInt();
-        double length = Math.sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1));
-        System.out.println("length of line is : " + length );
-   
-		
-	}
-
+	public static void main(String[] args) { 
+	System.out.println("welcome to line comparison computation program on master branch");
+    Scanner ch = new Scanner(System.in);
+    System.out.println("enter the coordinates of x1 & y1 : ");
+    int x1 = ch.nextInt();
+    int y1 = ch.nextInt();
+    System.out.println("enter the coordinates of x2 & y2 : ");
+    int x2 = ch.nextInt();
+    int y2 = ch.nextInt();
+    
+    
+    System.out.println("enter the coordinates of x3 & y3 : ");
+    int x3 = ch.nextInt();
+    int y3 = ch.nextInt();
+    System.out.println("enter the coordinates of x4 & y4 : ");
+    int x4 = ch.nextInt();
+    int y4 = ch.nextInt();
+    double length1 = Math.sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1));
+    double length2 = Math.sqrt((x4-x3) * (x4-x3) + (y4-y3) * (y4-y3));
+    System.out.println("first line length is : " + length1 +  " units" );
+    System.out.println("second line length is : " + length2 + " units" );
+        System.out.println();
+    if(length1 == length2)
+        System.out.println("both lines are equal");
+    else
+   	 System.out.println("both lines are not equal");
+    }
 }
+
+
